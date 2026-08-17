43dd389a79d4ef2225147c86d66c09f2182e311f
Gabriel <gabriel_giacomelli@estudante.sesisenai.org.br>
2026-08-17 11:03:39 -0300

    up - index

--- index.php
+++ index.php
@@ -1,4 +1,46 @@
-<?php 
-echo "Hello, World!";
-?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">My Website</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container mt-5">
+            <h1>Bem vindo ao Site de Controle de Pratos!</h1>
+            <p>Aqui você encontra informações sobre nossos cadastros de clientes e pratos!</p>
+        </div>
+        <div class="container mt-3">
+         <a href="outrapagina.html" class="btn btn-primary" role="button">Ir para outra página</a>      
+        <a href="outrapagina.html" class="btn btn-primary" role="button">Ir para outra página</a>
 
+        </div>
+    </main>
+</body>
+
+</html>
